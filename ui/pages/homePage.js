6baab724bc165b2d7a6670791e5ac4a0020d9b2b
jonas-martinez <cbobbxta.fr@gmail.com>
2021-10-14 11:53:59 +0200

    basic page

--- ui/pages/homePage.js
+++ ui/pages/homePage.js
@@ -3,29 +3,80 @@ const actions = require("../../listeners/actions")
 module.exports = function homePage(data) {
     return {
         type: "flex",
-        direction: "row",
+        direction: "col",
         spacing: 4,
+        fillParent: true,
+        crossAxisAlignment: "stretch",
         children: [
+            {
+                type: "styledContainer",
+                color: 0xFFFFFFFF,
+
+                child: {
+                    type: "flex",
+                    direction: "row",
+                    fillParent: true,
+                    mainAxisAlignment: "spaceBetween",
+                    padding: {
+                        left: 2,
+                        right: 2,
+                        top: 2,
+                        bottom: 2,
+                    },
+                    children: [
+                        {
+                            type: "flex",
+                            direction: "row",
+                            children: [
+                                {
+                                    type: "text",
+                                    style: "headline1",
+                                    value: "BBoost"
+                                }
+                            ]
+                        },
+                        {
+                            type: "flex",
+                            direction: "row",
+                            children: [
+                                {
+                                    type: "text",
+                                    style: "headline1",
+                                    value: "Lenra"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
             {
                 type: "flex",
                 direction: "col",
+                spacing: 2,
+                padding: {
+                    left: 2,
+                    right: 2
+                },
                 children: [
                     {
-                        type: "image",
-                        path: "appicon.png"
+                        type: "text",
+                        style: "headline1",
+                        value: "Applications utilisées:"
                     },
                     {
-                        type: "button",
-                        text: `click ${data.value}!`,
-                        onPressed: {
-                            action: actions.SET_VALUE,
-                            props: {
-                                value: "Me",
+                        type: "flex",
+                        direction: "row",
+                        children: [
+                            {
+                                type: "text",
+                                value: "foo"
                             }
-                        }
+                        ]
                     }
                 ]
             }
+
         ]
     }
+
 }
